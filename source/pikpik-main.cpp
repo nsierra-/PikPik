d9b1d0a90c25dafddfda291a07ab9042addf7eb6
Noé Sierra Velasquez <noesierravelasquez@gmail.com>
2020-10-08 03:50:09 +0200

    Custom message handling

--- source/pikpik-main.cpp
+++ source/pikpik-main.cpp
@@ -1,13 +1,33 @@
 #include "PikWindow.h"
 #include <QApplication>
-#include <QLabel>
-#include <QWidget>
+#include <QtGlobal>
+#include "stdio.h"
+#include "stdlib.h"
+
+void pikMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	QByteArray localMsg = msg.toLocal8Bit();
+	(void)context;
+
+	switch (type)
+	{
+		case QtFatalMsg:
+			fprintf(stderr, "Fatal Error: %s\n", localMsg.constData());
+			abort();
+
+		default:
+			fprintf(stderr, "%s\n", localMsg.constData());
+			break;
+	}
+}
 
 int main(int argc, char *argv[ ])
 {
+	qInstallMessageHandler(pikMessageHandler);
 	QApplication app(argc, argv);
 	PikWindow window;
 
-	window.showMaximized();
+	window.resize(400, 400);
+	window.show();
 	return app.exec();
 }
